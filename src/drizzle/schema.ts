20e58570aeb7f0d3c4f251ea72055f4cca1f8bab
Colin Eckert <colineckert10@gmail.com>
2025-08-23 13:49:36 -0700

    Add user notification settings and resume tables

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,1 +1,7 @@
 export * from "@/drizzle/schema/user";
+export * from "./schema/organization";
+export * from "./schema/jobListing";
+export * from "./schema/userNotificationSettings";
+export * from "./schema/userResume";
+export * from "./schema/jobListingApplication";
+export * from "./schema/organizationUserSettings";
